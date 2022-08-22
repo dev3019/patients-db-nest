86f24fbb5a2584e244168fa92e7e2631880fed86
Divyansh <devash@eiysys.in>
2022-08-22 12:45:04 -0400

    MongoDB integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patients/patient.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PatientModule],
+  imports: [
+    PatientModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://div_api-practice:google11@cluster0.r99se.gcp.mongodb.net/patients-db?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
